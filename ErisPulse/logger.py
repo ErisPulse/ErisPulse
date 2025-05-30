9a7369f6c6734a91f043ca55d308f6d0e06d76d2
wsu2059q <wsu2059@qq.com>
2025-05-30 20:05:30 +0800

    feat: 添加日志保存功能并更新版本号至1.0.14.dev1

--- ErisPulse/logger.py
+++ ErisPulse/logger.py
@@ -1,8 +1,10 @@
 import logging
 import inspect
+import datetime
 
 class Logger:
     def __init__(self):
+        self._logs = {}
         self._logger = logging.getLogger("ErisPulse")
         self._logger.setLevel(logging.DEBUG)
         if not self._logger.handlers:
@@ -14,6 +16,28 @@ class Logger:
         level = level.upper()
         if hasattr(logging, level):
             self._logger.setLevel(getattr(logging, level))
+
+    def save_logs(self, path: str):
+        if self._logs == None:
+            self._logger.warning("没有log记录可供保存。")
+            return
+        try:
+            with open(path, "w") as file:
+                for module, logs in self._logs.items():
+                    file.write(f"Module: {module}\n")
+                    for log in logs:
+                        file.write(f"  {log}\n")
+            self._logger.info(f"日志已被保存到：{path}。")
+        except Exception as e:
+            self._logger.error(f"无法保存日志到 {path}: {e}。")
+            raise e
+
+    def _save_in_memory(self, ModuleName, msg):
+        if ModuleName not in self._logs:
+            self._logs[ModuleName] = []
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        msg = f"{timestamp} - {msg}"
+        self._logs[ModuleName].append(msg)
 
     def _get_caller(self):
         frame = inspect.currentframe().f_back.f_back
@@ -27,22 +51,27 @@ class Logger:
 
     def debug(self, msg, *args, **kwargs):
         caller_module = self._get_caller()
+        self._save_in_memory(caller_module, msg)
         self._logger.debug(f"[{caller_module}] {msg}", *args, **kwargs)
 
     def info(self, msg, *args, **kwargs):
         caller_module = self._get_caller()
+        self._save_in_memory(caller_module, msg)
         self._logger.info(f"[{caller_module}] {msg}", *args, **kwargs)
 
     def warning(self, msg, *args, **kwargs):
         caller_module = self._get_caller()
+        self._save_in_memory(caller_module, msg)
         self._logger.warning(f"[{caller_module}] {msg}", *args, **kwargs)
 
     def error(self, msg, *args, **kwargs):
         caller_module = self._get_caller()
+        self._save_in_memory(caller_module, msg)
         self._logger.error(f"[{caller_module}] {msg}", *args, **kwargs)
 
     def critical(self, msg, *args, **kwargs):
         caller_module = self._get_caller()
+        self._save_in_memory(caller_module, msg)
         self._logger.critical(f"[{caller_module}] {msg}", *args, **kwargs)
 
 logger = Logger()
