9ad79431e6191b4e6f3e491bb9a3af66b717f9ef
wsu2059q <wsu2059@qq.com>
2025-07-07 23:26:27 +0800

    feat(example): 新增适配器示例模块及基础框架实现

---
+++ examples/example-module/MyModule/Core.py
@@ -0,0 +1,33 @@
+# 你也可以直接导入对应的模块
+# from ErisPulse import sdk
+# from ErisPulse.Core import logger, env, raiserr, adapter
+
+class Main:
+    def __init__(self, sdk):    # 这里也可以不接受sdk参数
+        self.sdk = sdk
+        self.env = self.sdk.env
+        self.logger = self.sdk.logger
+        
+        self.logger.info("MyModule 初始化完成")
+        self.load_config()
+
+    def load_config(self):
+        self.config = self.env.get("MyModule", {})
+
+        if self.config is None:
+            self.logger.error("无法加载配置文件")
+
+            self.env.setConfig("MyModule", {
+                "key1": "value1",
+                "key2": ["value2", "value3"],
+                "key3": {
+                    "key4": "value4",
+                    "key5": "value5"
+                },
+                "key6": True
+            })
+            self.logger.error("已生成对应的配置内容到你的env.py中, 建议修改后再次运行")
+
+    def hello(self):
+        self.logger.info("Hello World!")
+        # 其它模块可以通过 sdk.MyModule.hello() 调用此方法
