115f3b5664ab9f698d1fdddf111d4c0e2bff04ce
wsu2059q <wsu2059@qq.com>
2025-07-18 21:44:55 +0800

    feat: 版本号更新至2.1.5，Core模块新增`AdapterBase`类，标记`raiserr`为弃用状态并移除核心部分对其依赖

--- src/ErisPulse/Core/__init__.py
+++ src/ErisPulse/Core/__init__.py
@@ -5,8 +5,10 @@ from .mods import mods
 from .raiserr import raiserr
 from .util import util
 from .server import adapter_server
+BaseAdapter = AdapterFather
 
 __all__ = [
+    'BaseAdapter',
     'AdapterFather',
     'SendDSL',
     'adapter',
