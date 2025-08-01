a3d596f1d0851cc04998e56d44605ed42ba934cd
wsu2059q <wsu2059@qq.com>
2025-08-01 14:59:25 +0800

    refactor: 移除Core模块中未使用的exceptions模块及相关导出

--- src/ErisPulse/Core/__init__.py
+++ src/ErisPulse/Core/__init__.py
@@ -2,7 +2,6 @@ from .adapter import AdapterFather, SendDSL, adapter
 from .env import env
 from .logger import logger
 from .mods import mods
-from .exceptions import exceptions
 from .router import router, adapter_server
 from .config import config
 BaseAdapter = AdapterFather
@@ -11,7 +10,6 @@ __all__ = [
     'BaseAdapter',
     'AdapterFather',
     'SendDSL',
-    'exceptions',
     'adapter',
     'env',
     'logger',
